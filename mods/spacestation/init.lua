5f922339b8228cb2c394cb566209ca74d4f6d635
Ryan Hanson <hansonry@gmail.com>
2016-06-23 17:37:16 -0500

    Added Lights

--- mods/spacestation/init.lua
+++ mods/spacestation/init.lua
@@ -27,3 +27,26 @@ minetest.register_node("spacestation:roof", {
 	sounds = default.node_sound_stone_defaults(),
 })
 
+minetest.register_node("spacestation:light", {
+	description = "Light",
+	drawtype = "signlike",
+	tiles ={"spacestation_light.png"},
+	inventory_image = "spacestation_light.png",
+	wield_image = "spacestation_light.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	is_ground_content = false,
+	walkable = false,
+	light_source = LIGHT_MAX-1,
+	selection_box = {
+		type = "wallmounted",
+		--wall_top = {-0.1, 0.5-0.6, -0.1, 0.1, 0.5, 0.1},
+		--wall_bottom = {-0.1, -0.5, -0.1, 0.1, -0.5+0.6, 0.1},
+		--wall_side = {-0.5, -0.3, -0.1, -0.5+0.3, 0.3, 0.1},
+	},
+	groups = {choppy=2,dig_immediate=3,attached_node=1},
+	--legacy_wallmounted = true,
+	sounds = default.node_sound_defaults(),
+})
+
