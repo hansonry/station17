ba91b712f1f93d71494cdc2055c5a3519ba00028
Ryan Hanson <hansonry@gmail.com>
2022-06-23 18:42:26 -0500

    WIP: still trying to square away this id computer menu

--- mods/spacestation/init.lua
+++ mods/spacestation/init.lua
@@ -567,6 +567,69 @@ minetest.register_craftitem("spacestation:programmer", {
    end,
 })
 
+local function formspec_builder(compTable, hori_spacing, virt_spacing, page_padding)
+   hori_spacing = hori_spacing or 0.1
+   virt_spacing = virt_spacing or 0.1
+   page_padding = page_padding or 0.5
+   local next_x = page_padding
+   local next_y = page_padding
+   local max_x = next_x
+   local formStr = ""
+   local f = string.format
+   for row_i, row_v in ipairs(compTable) do
+      next_x = page_padding
+      local row_height = 0
+      if type(row_v) == "string" and row_v == "player_inventory" then
+         formStr = formStr .. 
+                   f("list[current_player;main;%.3f,%.3f;8,4;]", 
+                     next_x, next_y) ..
+                   "listring[]"
+         row_height = 4 
+      else
+         for cell_i, cell_v in ipairs(row_v) do
+            local cell_height = 0
+            local cellType = cell_v[1]
+            if cellType == "label" then
+               local width = cell_v[2]
+               local text  = cell_v[3]
+               formStr = formStr ..
+                         f("label[%.3f,%.3f;%s]", 
+                           next_x, next_y, text)
+               next_x = next_x + width
+               cell_height = 1
+            elseif cellType == "list" then
+               local width    = cell_v[2]
+               local height   = cell_v[3]
+               local location = cell_v[4]
+               local name     = cell_v[5]
+               formStr = formStr ..
+                         f("list[%s;%s;%.3f,%.3f;%d,%d;]",
+                           location, name, next_x, next_y, width, height)
+               next_x = next_x + width
+               cell_height = height
+            elseif cellType == "field" then
+               local width = cell_v[2]
+               local name  = cell_v[3]
+               local text  = cell_v[4]
+               formStr = formStr ..
+                         f("field[%.3f,%.3f;%.3f,%.3f;%s;;%s]",
+                           next_x, next_y, width, 0.5, name, text)
+               next_x = next_x + width
+               cell_height = 0.5
+            end
+            next_x = next_x + virt_spacing
+            if cell_height > row_height then row_height = cell_height end
+         end
+      end
+      next_y = next_y + row_height + hori_spacing
+      if next_x > max_x then max_x = next_x end
+   end
+   
+   return "formspec_version[5]" ..
+          f("size[%.3f,%.3f]", max_x + page_padding, next_y + page_padding) ..
+          formStr
+end
+
 local btn_text_set   = "Set"
 local btn_text_reset = "Reset"
 local btn_text_clear = "Clear"
@@ -605,8 +668,28 @@ local function computer_idcard_build_formspec(inventory)
 
    --print("List:  " .. perm_list .. "\n")
 
+   local data = {
+      {
+         {"label", 1.5, "User ID:"},
+         {"list",  1, 1, "context", "user_id"},
+         {"label", 1, "Name:"},
+         {"label", 3, user_name},
+         {"label", 1, "Job:"},
+         {"label", 4, user_job_title}, 
+      },
+      {
+         {"label", 1.5, "Target ID:"},
+         {"list",  1, 1, "context", "target_id"},
+         {"label", 1, "Name:"},
+         {"field", 3, "target_name", target_name},
+         {"label", 1, "Job:"},
+         {"field", 4, "target_job_title", target_job_title}, 
+      },
+      "player_inventory"
+   }
+   local spec = formspec_builder(data)
 
-   local spec = "formspec_version[5]" ..
+   local spec2 = "formspec_version[5]" ..
                 "size[12,10]" ..
                 "label[1,1;User ID:]" ..
                 "list[context;user_id;2.1,0.5;1,1;]" ..
